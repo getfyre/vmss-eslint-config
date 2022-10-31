ba58f3e73f0798e3ffc219444857a1e490ef9f80
Grant-Rigdon <42386383+Grant-Rigdon@users.noreply.github.com>
2022-10-31 14:03:30 -0400

    FYRE-8984: fixed semi rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -25,7 +25,7 @@ module.exports = {
             {
                 trailingComma: 'none',
                 tabWidth: 4,
-                semi: false,
+                semi: true,
                 singleQuote: true,
                 bracketSpacing: true
             }
